6026b48874a010dfb281931d8decf7e651b75f5d
Nous Mobility <nousmfm@gmail.com>
2019-01-07 12:54:05 +0530

    UI design updated.

--- LifeBenefits/LifeBenefits/View/BenefitsDetailPage.xaml.cs
+++ LifeBenefits/LifeBenefits/View/BenefitsDetailPage.xaml.cs
@@ -12,18 +12,20 @@ namespace LifeBenefits.View
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class BenefitsDetailPage : ContentPage
 	{
-        private bool isExpanded;
+        private bool isExpanded=true;
 		public BenefitsDetailPage (string title)
 		{
 			InitializeComponent ();
             this.Title = title;
             isExpanded = true;
+            expnandImage.Source = "minus";
         }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
             isExpanded = !isExpanded;
             expandableList.IsVisible = isExpanded;
+            expnandImage.Source = isExpanded ? "minus" : "plus";
         }
     }
 }
