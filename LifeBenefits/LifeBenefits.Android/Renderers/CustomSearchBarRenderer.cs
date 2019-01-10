10deee7516331bc71c331c5c0576c5620185bd1a
Nous Mobility <nousmfm@gmail.com>
2019-01-10 14:44:46 +0530

    SearchBar for tablet improved.

---
+++ LifeBenefits/LifeBenefits.Android/Renderers/CustomSearchBarRenderer.cs
@@ -0,0 +1,31 @@
+﻿using Android.Content;
+using Android.OS;
+using LifeBenefits.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(SearchBar), typeof(CustomSearchBarRenderer))]
+namespace LifeBenefits.Droid.Renderers
+{
+    public class CustomSearchBarRenderer : SearchBarRenderer
+    {
+        public CustomSearchBarRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.N)
+            {
+                Element.HeightRequest = 42;
+            }
+        }
+    }
+}
